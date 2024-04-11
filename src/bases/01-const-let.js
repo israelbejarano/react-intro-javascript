2efff28ece621c3a2602002b8019e9abbce95fdb
israel <israel.bejarano.a@gmail.com>
2024-04-11 06:26:46 +0200

    feat: const y let

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y constantes
+
+const nombre = 'Israel';
+const apellido = 'Bejarano';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    const nombre = 'Juan';
+    let valorDado = 6;
+    console.log( nombre, valorDado);
+}
+
+console.log(nombre, valorDado);
