615a712dabeea9a562009e14d961ab43d6d3272b
israel <israel.bejarano.a@gmail.com>
2024-04-23 06:29:22 +0200

    feat: promesas

--- src/bases/09-promesas.js
+++ src/bases/09-promesas.js
@@ -1,13 +1,31 @@
 // promesas
 import { getHeroeById } from './08-imp-exp';
 
-const promesa = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const heroe = getHeroeById(2);
-        resolve(heroe);
-    }, 2000);
-});
+// const promesa = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const heroe = getHeroeById(2);
+//         resolve(heroe);
+//     }, 2000);
+// });
 
-promesa.then((heroe) => {
-    console.log(heroe);
-}).catch(err => console.error(err));
+// promesa.then((heroe) => {
+//     console.log(heroe);
+// }).catch(err => console.error(err));
+
+const getHeroeByIdAsync = (id) => {
+
+    const promesa = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const heroe = getHeroeById(id);
+            if (heroe) {
+                resolve(heroe);
+            }
+            else {
+                reject('No se pudo encontrar el heroe')
+            }
+        }, 2000);
+    });
+    return promesa
+}
+
+getHeroeByIdAsync(4).then(heroe => console.log('Heroe: ', heroe)).catch(err => console.warn(err));
