2568901d46ab747798fffba3270cde2e6802eb3b
israel <israel.bejarano.a@gmail.com>
2024-04-26 06:33:38 +0200

    feat: peticiones con fetch

--- src/bases/10-fecth-api.js
+++ src/bases/10-fecth-api.js
@@ -2,3 +2,14 @@ console.log('Hola mundo');
 
 const appKey = '';
 const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${appKey}`);
+
+peticion
+    .then(resp => resp.json())
+    .then(({data}) => {
+        const {url} = data.images.url
+        console.log(url);
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(url);
+    })
+    .catch(console.warn);
